8fb6841661048888c307ca605a26060668e81790
Vance Tran <v@vancetran.com>
2014-05-20 22:36:38 -0700

    rudimentary build process

--- grunt/less.js
+++ grunt/less.js
@@ -6,30 +6,7 @@ module.exports = {
 			ieCompat: true
 		},
 		files: {
-			"<%= appconfig.src %>/css/bootstrap-ucop.css": "<%= appconfig.src %>/less/bootstrap-ucop.less",
-			"<%= appconfig.src %>/css/bootstrap-ucnet.css": "<%= appconfig.src %>/less/bootstrap-ucnet.less"
-		}
-	},
-	dist: {
-		options: {
-			paths: ["<%= appconfig.src %>/less/"],
-			cleancss: true,
-			ieCompat: true
-		},
-		files: {
-			"<%= appconfig.dest %>/css/bootstrap-ucop-min.css": "<%= appconfig.src %>/less/bootstrap-ucop.less",
-			"<%= appconfig.dest %>/css/bootstrap-ucnet-min.css": "<%= appconfig.src %>/less/bootstrap-ucnet.less"
-		}
-	},
-	noMin: {
-		options: {
-			paths: ["<%= appconfig.src %>/less/"],
-			cleancss: false,
-			ieCompat: true
-		},
-		files: {
-			"<%= appconfig.dest %>/css/bootstrap-ucop.css": "<%= appconfig.src %>/less/bootstrap-ucop.less",
-			"<%= appconfig.dest %>/css/bootstrap-ucnet.css": "<%= appconfig.src %>/less/bootstrap-ucnet.less"
+			"<%= appconfig.src %>/css/main.css": "<%= appconfig.src %>/less/bootstrap.less"
 		}
 	}
 };
