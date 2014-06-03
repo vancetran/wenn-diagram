da3fe3bab8f9a9cd5e677bfb928bac1ab99afe42
Vance Tran <vancetran@gmail.com>
2014-06-03 08:02:39 -0700

    Server refinement

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var connect = require('connect'),
   open = require('open'),
-  port = process.argv[2] || 8080;
+  port = process.argv[3] || 8080,
+  serverType = process.argv[2] || 'app';
 
-connect().use(connect.static(__dirname+"/build")).listen(port);
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+connect().use(connect.static(__dirname+'/'+serverType)).listen(port);
+console.log("Static file server running at\n  => http://localhost:" + port + "/\n Pointing to /"+ serverType +" folder.\n CTRL + C to shutdown");
 open('http://localhost:' + port);
