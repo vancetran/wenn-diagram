0e1fcc4a8f27ab830f3c47cb8f848cdc1ba36f00
Vance Tran <vancetran@gmail.com>
2014-06-02 21:38:51 -0700

    Some initial styling and temporary graphics

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var connect = require('connect'),
-    open = require('open'),
-	port = process.argv[2] || 8080;
+  open = require('open'),
+  port = process.argv[2] || 8080;
 
 connect().use(connect.static(__dirname+"/app")).listen(port);
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
