0f52908bde75b4b5cd0a9a018f7de0243410070f
Vance Tran <vancetran@gmail.com>
2014-06-04 22:01:25 -0700

    Fixed imagemin by deleting and reinstalling it

--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -2,8 +2,9 @@ module.exports = {
 	build: {
 		files: [{
 			expand: true,
+			progressive: true,
 			cwd: '<%= appconfig.src %>/images/',
-			src: ['**/*.{png,jpg,jpeg,gif}'],
+			src: ['**/*.{png,jpg,gif}'],
 			dest: '<%= appconfig.dest %>/images/'
 		}]
 	}
