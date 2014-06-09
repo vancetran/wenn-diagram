f500ea8c32df78740e74142bac5ca760bcbb19ca
vancetran <v@vancetran.com>
2014-06-08 18:15:03 -0700

    Testing out Express

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var connect = require('connect'),
+var express = require('express'),
   open = require('open'),
   port = process.argv[3] || 8080,
   serverType = process.argv[2] || 'app';
 
-connect().use(connect.static(__dirname+'/'+serverType)).listen(port);
+express().use(express.static(__dirname+'/'+serverType)).listen(port);
 console.log("Static file server running at\n  => http://localhost:" + port + "/\n Pointing to /"+ serverType +" folder.\n CTRL + C to shutdown");
 open('http://localhost:' + port);
