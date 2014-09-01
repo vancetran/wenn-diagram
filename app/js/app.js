9c6d576c793c5ea6bd7836aad563a10d4d75912a
Vance Tran <vancetran@gmail.com>
2014-08-31 17:53:51 -0700

    major progress with fancy grid

---
+++ app/js/app.js
@@ -0,0 +1,30 @@
+var app = app || {};
+
+$( document ).ready(function() {
+	app.leaflet();
+	app.doMasonry();
+});
+
+app.leaflet = function() {
+  var map = L.map('map').setView([32.869000, -117.252944], 10);
+
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    id: 'caverntan.j9lbmc08',
+    detectRetina: true
+  }).addTo(map);
+
+  L.marker([32.869000, -117.252944]).addTo(map)
+    .bindPopup("<strong>Martin Johnson House</strong><br>8840 Biological Grade<br>La Jolla, CA, 92037<br><a href='https://goo.gl/maps/aoGAA'>Get Directions</a>").openPopup();
+
+  var popup = L.popup();
+  map.scrollWheelZoom.disable();
+}
+
+app.doMasonry = function(){
+	var $msnryContainer = $('.masonry');
+
+	imagesLoaded( $msnryContainer, function() {
+		$msnryContainer.masonry();
+	});
+}
--- server.js
+++ server.js
@@ -5,4 +5,4 @@ var express = require('express'),
 
 express().use(express.static(__dirname+'/'+serverType)).listen(port);
 console.log("Static file server running at\n  => http://localhost:" + port + "/\n Pointing to /"+ serverType +" folder.\n CTRL + C to shutdown");
-open('http://localhost:' + port);
+// open('http://localhost:' + port);
