9bd518d817d5c66b9991aa909be0e32c5a697f1f
Vance Tran <vancetran@gmail.com>
2014-09-07 19:55:57 -0700

    RSVP linkage, calendar icon, cleanup

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
 
 app.leaflet = function() {
   var map = L.map('map').setView([32.869000, -117.252944], 10);
-
+  L.DomEvent.preventDefault = function(e) {return;}
   L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
     maxZoom: 18,
     id: 'caverntan.j9lbmc08',
@@ -18,6 +18,8 @@ app.leaflet = function() {
     .bindPopup("<strong>Martin Johnson House</strong><br>8840 Biological Grade<br>La Jolla, CA, 92037<br><a href='https://goo.gl/maps/aoGAA'>Get Directions</a>").openPopup();
 
   var popup = L.popup();
+  map.dragging.disable();
+  map.touchZoom.disable();
   map.scrollWheelZoom.disable();
 }
 
